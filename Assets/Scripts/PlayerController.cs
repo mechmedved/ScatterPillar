e2db014bf72e7e58781b8b4b0962bc2be2b691e7
Bird <mail@birdlab.ru>
2021-09-04 12:05:26 +0300

    test commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+
     public float speed = 5.0f;
 
     // Start is called before the first frame update
